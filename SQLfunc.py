bb81f7e8abe78e3c2ce42a6a7fcadbba9eceb39e
jack-o-lantern2099 <58605071+jack-o-lantern2099@users.noreply.github.com>
2020-10-02 01:53:30 +0530

    Add files via upload

---
+++ SQLfunc.py
@@ -0,0 +1,34 @@
+def WRITE_SCORE(x,s):
+    import mysql.connector
+    mydb=mysql.connector.connect(host="localhost",user="root",password="HG@18102003",database="PYTHON")
+    mycursor=mydb.cursor()
+    sqlform="INSERT INTO PHIGH(Name,Score) VALUES(%s,%s)"
+    info=(x,s)
+    mycursor.execute(sqlform,info)
+    mydb.commit()
+
+def LEADERBOARD():
+    import mysql.connector
+    mydb=mysql.connector.connect(host="localhost",user="root",password="HG@18102003",database="PYTHON")
+    mycursor=mydb.cursor()
+    mycursor.execute("SELECT * FROM PHIGH ORDER BY Score")
+    leader = mycursor.fetchall()
+    for s in leader:
+        print(s)
+
+def CREATE():
+    import mysql.connector
+    mydb=mysql.connector.connect(host="localhost",user="root",password="HG@18102003",database="PYTHON")
+    mycursor=mydb.cursor()
+    try:
+        mycursor.execute("SHOW TABLES")
+        records=mycursor.fetchall()
+        if 'PHIGH' in records:
+            x=1
+    except:
+        mycursor.execute("CREATE TABLE PHIGH(Name varchar(20) NOT NULL PRIMARY KEY,Score int(3) NOT NULL )")
+        print("leader board created")
+    mydb.commit()
+    
+
+
